8893d3626374cbae973749a0d6671e435e087471
Haqverdi56 <qhaqverdi56@gmail.com>
2023-07-28 19:17:54 +0400

    Accordion update

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 const factList = document.querySelector('.factList');
+const container = document.querySelectorAll('.container')
 
-function getFacts() {
+
+function getFacts(div) {
     const factItem = document.createElement('div');
     const factDesription = document.createElement('div');
+    const divLabel = document.createElement('div')
     factItem.className = 'factItem'
-    factDesription.className = 'factDesription'
-
+    factDesription.className = 'content'
+    divLabel.className = 'label'
+    divLabel.innerText = 'Fact'
+    
     
     fetch('https://catfact.ninja/fact')
     .then(res => res.json())
     .then(response => {
-        console.log(factDesription);
         factDesription.innerText = response.fact
     })
     factItem.appendChild(factDesription);
-    factList.appendChild(factItem);
+    div.appendChild(divLabel);
+    div.appendChild(factDesription);
 }
 
-getFacts();
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    console.log('this', this)
+  })
+}
+
+container.forEach(div => {
+    div.addEventListener('click', function() {
+        div.innerText = ''
+        getFacts(div);
+    })
+})
